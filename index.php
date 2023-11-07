efcef8be7d5c027e8e02736adc2aa42dd95b7b2d
eliard0 <eliardosilva81@gmail.com>
2023-11-07 16:40:33 -0300

    conhecendo o slim muito parecido com laravel, feat implementando metodo post

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use SplitPlay\Controllers\ProdutosController\ProdutoController;
 
 require_once "vendor/autoload.php";
 
@@ -6,6 +9,13 @@ $app = new \Slim\App();
 
 $app->get('/', function ($request, $response, $args) {
     return $response->withStatus(200)->write('Hello World! ola mundo');
+});
+
+$app->post('/produto', function ($request, $response, $args) {
+    $data = $request->getParsedBody();
+    print_r($data);die;
+    // $nome = $args['nome'];
+    // return $response->withStatus(200)->write("{$nome} ola mundo");
 });
 
 $app->run();
